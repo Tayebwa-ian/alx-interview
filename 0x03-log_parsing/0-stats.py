00c16f504fe95502f577700d2180783a111327e3
Tayebwa-ian <tayebwaian0@gmail.com>
2024-06-20 22:39:29 +0200

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,39 +2,40 @@
 """
 Log Parsing Script
 """
-import sys
-import re
-
-
-def line_checker(line: str) -> list:
-    """
-    Check the line if it comforms with certain predefined rules
-    Arg:
-        line: line to check
-    """
-    pattern = r'\"GET /projects/260 HTTP/1.1\"'
-    match = re.search(pattern, line)
-    line_split_list = line.split(" ")
-    if match and len(line_split_list) > 7:
-        return line_split_list
-    return []
-
-
-def printer(codes_dict: dict, file_size: int) -> None:
-    """
-    Print output to the stdout
-    Arg:
-        codes_int: A dict with codes and number of times their appear
-        files_size: cumulative total size of the file
-    """
-    print("File size: {}".format(file_size))
-    ordered_codes = [int(key) for key in codes_dict.keys()]
-    ordered_codes.sort()
-    for code in ordered_codes:
-        print("{}: {}".format(code, codes_dict[str(code)]))
-
 
 if "__main__" == __name__:
+
+    import sys
+    import re
+
+
+    def line_checker(line: str) -> list:
+        """
+        Check the line if it comforms with certain predefined rules
+        Arg:
+            line: line to check
+        """
+        pattern = r'\"GET /projects/260 HTTP/1.1\"'
+        match = re.search(pattern, line)
+        line_split_list = line.split(" ")
+        if match and len(line_split_list) > 7:
+            return line_split_list
+        return []
+
+
+    def printer(codes_dict: dict, file_size: int) -> None:
+        """
+        Print output to the stdout
+        Arg:
+            codes_int: A dict with codes and number of times their appear
+            files_size: cumulative total size of the file
+        """
+        print("File size: {}".format(file_size))
+        ordered_codes = [int(key) for key in codes_dict.keys()]
+        ordered_codes.sort()
+        for code in ordered_codes:
+            print("{}: {}".format(code, codes_dict[str(code)]))
+
 
     file_size: int = 0
     possible_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
