a5d9683a1a22e2ca1c5f06ba5b9477ec73ea5022
Tayebwa-ian <tayebwaian0@gmail.com>
2024-06-20 21:39:00 +0200

    Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Log Parsing
+"""
+import sys
+import re
+
+
+def line_checker(line: str) -> list:
+    """
+    Check the line if it comforms with certain predefined rules
+    Arg:
+        line: line to check
+    """
+    pattern = r'\"GET /projects/260 HTTP/1.1\"'
+    match = re.search(pattern, line)
+    if match:
+        return line.split(" ")
+    return []
+
+
+def printer(codes_dict: dict, file_size: int) -> None:
+    """
+    Print output to the stdout
+    Arg:
+        codes_int: A dict with codes and number of times their appear
+        files_size: cumulative total size of the file
+    """
+    print(f"File size: {file_size}")
+    ordered_codes = [int(key) for key in codes_dict.keys()]
+    ordered_codes.sort()
+    for code in ordered_codes:
+        print(f"{code}: {codes_dict[str(code)]}")
+
+
+if "__main__" == __name__:
+    file_size: int = 0
+    possible_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    codes: dict = {}
+    tally: int = 0
+    try:
+        for line in sys.stdin:
+            line: str = line.rstrip()
+            line_list: list = line_checker(line)
+            if len(line_list) > 7:
+                tally += 1
+                file_size += int(line_list[-1])
+                if line_list[-2] in possible_codes:
+                    if codes.get(line_list[-2]):
+                        codes[line_list[-2]] += 1
+                    else:
+                        codes[line_list[-2]] = 1
+                if tally == 10:
+                    printer(codes, file_size)
+                    tally = 0
+                    codes.clear()
+    except KeyboardInterrupt as err:
+        printer(codes, file_size)
+        raise
