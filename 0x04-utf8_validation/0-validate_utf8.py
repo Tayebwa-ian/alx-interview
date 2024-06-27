108bcf6ac64c1bdfd2188d4df53f15870d732001
Tayebwa-ian <tayebwaian0@gmail.com>
2024-06-27 19:35:16 +0200

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,12 +10,17 @@ def validUTF8(data):
         data: a list of integers representing the data
     """
     data_length = len(data)
-    if data[0] > 127 and data_length < 2:
+    if data[0] < 128 and data_length == 1:
+        return True
+    elif data[0] > 127 and data_length < 2:
         return False
-    elif data[0] > 127 and data[0] < 248:
-        for i in data[1:]:
-            if i > 191 or i < 128:
-                return False
     elif data[0] > 247:
         return False
-    return True
+    elif data[0] > 127 and data[0] < 248:
+        for i in data[1:4]:
+            if i > 191 or i < 128:
+                return False
+    if data[4:]:
+        return validUTF8(data[4:])
+    else:
+        return True
