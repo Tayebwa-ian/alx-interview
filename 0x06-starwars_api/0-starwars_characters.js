7caba33bf951f044f45312299d38f2edb22e647a
Tayebwa-ian <tayebwaian0@gmail.com>
2024-07-13 22:07:40 +0200

    Star war Characters in order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,21 +5,32 @@ const request = require('request');
 
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 const resource = url + process.argv.slice(2)[0];
-request(resource, (err, response, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    const content = JSON.parse(body);
-    const cast = content.characters;
-    for (let i = 0; i < cast.length; i++) {
-      request(cast[i], (er, resp, body2) => {
-        if (er) {
-          console.log('Error: can not fetch a character');
-        } else {
-          const cont = JSON.parse(body2);
-          console.log(cont.name);
-        }
-      });
-    }
+
+function fetchUrl (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, resp, body) => {
+      if (err) {
+        console.log('Error: can not fetch a character');
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
+
+async function getCast (resource) {
+  const body = await fetchUrl(resource);
+  const urls = JSON.parse(body).characters;
+  try {
+    const results = await Promise.all(urls.map(url => fetchUrl(url)));
+    results.forEach((result, index) => {
+      const character = JSON.parse(result);
+      console.log(character.name);
+    });
+  } catch (err) {
+    console.error(err);
   }
-});
+}
+
+getCast(resource);
