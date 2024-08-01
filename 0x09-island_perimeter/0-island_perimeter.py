3ba008623e766de3fe52a498410fcc069dc45e88
Tayebwa-ian <tayebwaian0@gmail.com>
2024-08-01 14:59:13 +0200

    calculating the parameter of an island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,19 +13,19 @@ def island_perimeter(grid):
     grid_len = len(grid)
     cells = 0
     if grid_len > 101:
-        return
+        return 0
     for i, list_el in enumerate(grid):
         inner_len = len(list_el)  # length of inner list
         if inner_len > 101 or inner_len == grid_len:
-            return
+            return 0
         for j, el in enumerate(list_el):
             if (i == 0 or i == grid_len - 1) and el == 1:
-                return
+                return 0
             elif (j == 0 or j == inner_len - 1) and el == 1:
-                return
+                return 0
             if el == 1 and (grid[i][j + 1] == 1
                             and grid[i + 1][j] == 1):
-                return
+                return 0
             if el == 1:
                 cells += 1
     paramater = (cells * 2) + 2
